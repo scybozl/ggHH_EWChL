1b70c6310319f3fd0b7a0f852c6ba5b062eb2cfb
Ludovic Scyboz (local) <scybozl@tplxlap244.nat.physics.ox.ac.uk>
2020-06-29 10:20:26 +0100

    removed test-base/ directory (which was the run directory for running on Draco)

--- test-base/merge.py
+++
@@ -1,8 +0,0 @@
-import os
-from subprocess import *
-
-input_to_merge=Popen('echo 1 ; ls -c1 pwg-*-NLO.top ; echo ""', stdout=PIPE, shell=True)
-merge=Popen('../shell/mergedata', stdin=input_to_merge.stdout, stdout=PIPE)
-input_to_merge.stdout.close()
-os.wait()
-os.system('mv fort.12 pwg-NLO.top')
