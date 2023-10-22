658f424229eefd86630f24c310de050dce848985
scybozl <ludovic.scyboz@physics.ox.ac.uk>
2023-10-22 14:03:16 +1100

    modified version of run.sh (with warmup phase)

--- testrun/run.sh
+++ testrun/run.sh
@@ -8,38 +8,53 @@ nxgriditeration=3
 
 if [ "$1" = "help" ] || [ "$1" = "" ]
 then
-    echo "*************************************************"
-    echo "-- Menu for running powheg --                    "
-    echo "Possible running options:                        "
-    echo "./run 0 : run in Higgs Effective Theory (HEFT)   "
-    echo "./run 1 : run in Born improved HEFT              "
-    echo "./run 2 : run in approx. full theory (FTapp)     "
-    echo "./run 3 : run in full theory (grids for virtual) "
+    echo "******************************************************"
+    echo "-- Menu for running powheg --                         "
+    echo "Possible running options:                             "
+    echo "./run warmup : run warmup phase (generates links/grid)"
+    echo "./run 0      : run in Higgs Effective Theory (HEFT)   "
+    echo "./run 1      : run in Born improved HEFT              "
+    echo "./run 2      : run in approx. full theory (FTapp)     "
+    echo "./run 3      : run in full theory (grids for virtual) "
     echo "*************************************************"
     exit
 else
     mode=$1
 fi
 
-function runthem {
+function warmup {
     export PYTHONPATH=$PWD:$PYTHONPATH
+    # creates symbolic links to the files that are needed
+    # for mtdep = 3
+    if [ ! -f events.cdf ]
+    then
+        ln -s ../Virtual/events.cdf events.cdf
+    fi
+    if [ ! -f creategrid.py ]
+    then
+        ln -s ../Virtual/creategrid.py creategrid.py
+    fi
+    if [ "`echo Virt_full_*E*.grid`" == "Virt_full_*E*.grid" ]
+    then
+        for grid in ../Virtual/Virt_full_*E*.grid; do ln -s $grid ${grid##*/}; done
+    fi
+
+    # get formatted coupling values
+    chhh=$(awk 'sub(/^chhh/,""){printf "%+.4E", $1}' powheg.input-save)
+    ct=$(awk 'sub(/^ct /,""){printf "%+.4E", $1}' powheg.input-save)
+    ctt=$(awk 'sub(/^ctt/,""){printf "%+.4E", $1}' powheg.input-save)
+    cg=$(awk 'sub(/^cggh /,""){printf "%+.4E", $1}' powheg.input-save)
+    cgg=$(awk 'sub(/^cgghh/,""){printf "%+.4E", $1}' powheg.input-save)
+
+    gridtemp="Virt_full_${chhh}_${ct}_${ctt}_${cg}_${cgg}.grid"
+
+    pythoncmd="import creategrid as cg; cg.combinegrids('${gridtemp}', ${chhh}, ${ct}, ${ctt}, ${cg}, ${cgg})"
+    python3 -c "$pythoncmd"
+}
+
+function runthem {
     for i in $(seq 1 $ncores)
     do
-	if [ "$1" = "3" ]
-	then
-	    if [ ! -f events.cdf ]
-	    then
-		ln -s ../Virtual/events.cdf events.cdf
-	    fi
-	    if [ ! -f creategrid.py ]
-	    then
-		ln -s ../Virtual/creategrid.py creategrid.py
-	    fi
-	    if [ "`echo Virt_full_*E*.grid`" == "Virt_full_*E*.grid" ]
-	    then
-		for grid in ../Virtual/Virt_full_*E*.grid; do ln -s $grid ${grid##*/}; done
-	    fi
-	fi
 	# launch powheg
 	echo $i | $prg > $logfile-$i.log 2>&1 &
     done
@@ -48,6 +63,21 @@ function runthem {
 prg=../pwhg_main
 
 begin=$(date +"%s")
+
+if [ "$mode" = "warmup" ] || [ "$mode" = "3" ]
+then
+    echo "***********************************************"
+    echo " stage warmup"
+    echo "***********************************************"
+
+    warmup
+    wait
+
+    if [ "$mode" = "warmup" ]
+    then
+        exit
+    fi
+fi
 
 parstage=1
 echo "***********************************************"
@@ -59,7 +89,7 @@ do
     cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
     cp powheg.input powheg.input-$parstage-$xgrid
     logfile=run-$parstage-$xgrid
-    runthem $mode
+    runthem
     wait
     rm powheg.input
 done
@@ -73,10 +103,9 @@ echo "***********************************************"
 cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
 cp powheg.input powheg.input-$parstage
 logfile=run-$parstage
-runthem $mode
+runthem
 wait
 rm powheg.input
-done
 
 termin=$(date +"%s")
 difftimelps=$(($termin-$begin))
@@ -88,10 +117,10 @@ echo "***********************************************"
 echo " stage " $parstage
 echo "***********************************************"
 
-cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/">powheg.input
+cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/">powheg.input
 cp powheg.input powheg.input-$parstage
 logfile=run-$parstage
-runthem $mode
+runthem
 wait
 rm powheg.input
 
@@ -101,10 +130,10 @@ echo "***********************************************"
 echo " stage " $parstage
 echo "***********************************************"
 
-cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/testplots.*/#testplots 1/">powheg.input
+cat powheg.input-save | sed "s/mtdep.*/mtdep $mode/ ; s/parallelstage.*/parallelstage $parstage/ ; s/xgriditeration.*/xgriditeration $xgrid/ ; s/testplots.*/#testplots 1/">powheg.input
 cp powheg.input powheg.input-$parstage
 logfile=run-$parstage
-runthem $mode
+runthem
 wait
 
 termin=$(date +"%s")
